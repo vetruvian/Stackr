cf90d09b53754f38c0fbe62956fc033351b2d426
vetruvian <cory.malstrom@yahoo.com>
2016-03-22 15:33:46 -0700

    gemfile change

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-
+## Heroku assets setting
+##config.serve_static_files = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,8 +12,7 @@ Bundler.require(*Rails.groups)
 module Stackr
   class Application < Rails::Application
 
-    ## Heroku assets setting
-    config.serve_static_files = true
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
