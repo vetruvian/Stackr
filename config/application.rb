9749feced80d07cedb771387aa74846e8af4bae4
vetruvian <cory.malstrom@yahoo.com>
2016-03-22 14:34:40 -0700

    Deleted SO Heroku asset settings

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-## Heroku assets setting
-config.serve_static_assets = true
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +10,10 @@ Bundler.require(*Rails.groups)
 
 module Stackr
   class Application < Rails::Application
+
+    ## Heroku assets setting
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
